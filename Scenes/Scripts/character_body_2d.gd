ba91ff97b1d6b437eea2145bfe95f1d7bc82dc71
Xilyani <Davidspadini@gmail.com>
2024-10-26 16:46:39 +0200

    lots of changes

---
+++ Scenes/Scripts/character_body_2d.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+var speed := 425
+var acceleration := 800
+var deceleration := 1000
+var axis := Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+
+	axis.x = Input.get_action_strength("move_right")-Input.get_action_strength("move_left")
+	axis.y = Input.get_action_strength("move_down")-Input.get_action_strength("move_up")
+
+	axis = axis.normalized() * speed
+
+	velocity = velocity.lerp(axis, 0.1)
+
+	move_and_slide()
