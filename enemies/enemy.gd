ea702ae92f86e1cef5d635ed6c0b943e64c4a0cf
ErikTheBerik <erik@ahlund.me>
2024-10-26 14:49:39 +0200

    enemy smooth walk

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -9,33 +9,45 @@ enum State {
 	Scared
 }
 
-@export var RENDER_POINTS: bool = true
-@export var RENDER_POINT_LINES: bool = true
+@export var MAX_SPEED: float = 100.0
+@export var ROTATION_SPEED: float = 5.0
+@export var alert_color: Color
+@export var vision_color: Color
 
+@export_group("Idle")
 @export var MIN_IDLE_TIME: float = 2.0
 @export var MAX_IDLE_TIME: float = 5.0
 
+@export_group("Talking")
 @export var MIN_TALK_TIME: float = 3.0
 @export var MAX_TALK_TIME: float = 8.0
 
+@export_group("Walking")
 @export var POINTS_A: Array[Vector2] = []
 @export var POINTS_B: Array[Vector2] = []
 
-@export var MAX_SPEED: float = 100.0
+@export_group("Debug")
+@export var RENDER_POINTS: bool = true
+@export var RENDER_POINT_LINES: bool = true
+
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@onready var vision_cone: VisionCone2D = $VisionCone2D
+@onready var vision_renderer: Polygon2D = $VisionCone2D/VisionConeRenderer
 
 var rng := RandomNumberGenerator.new()
-
 var pointRadius: float = 10.0;
 
 var m_State: State = State.Idle;
 var m_Timer: float = 0.0;
 var m_GoingToA: bool = true;
-var m_WalkingToPos: Vector2 = Vector2.ZERO;
+var m_Direction: Vector2 = Vector2.ZERO;
 
 func _ready() -> void:
+	m_Direction = Vector2.RIGHT;
+	vision_renderer.color = vision_color
 	InputMap.load_from_project_settings()
 	
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if Engine.is_editor_hint():
 		queue_redraw();
 		var nodes := EditorInterface.get_selection().get_selected_nodes();
@@ -50,6 +62,8 @@ func _process(delta: float) -> void:
 				HandleNewPosition(POINTS_B)
 		return;
 		
+	RotateConeVision();
+	
 	match m_State:
 		State.Idle:
 			ProcessIdle(delta);
@@ -60,7 +74,10 @@ func _process(delta: float) -> void:
 		State.Scared:
 			return;
 			
-	pass;
+	move_and_slide()
+
+func RotateConeVision() -> void:
+	vision_cone.rotation = m_Direction.angle() + deg_to_rad(270)
 
 func _draw() -> void:
 	if Engine.is_editor_hint():
@@ -102,11 +119,8 @@ func SetWalkingPosition(somePoints: Array[Vector2]) -> void:
 		return;
 	
 	var index := rng.randi_range(0, size-1);
-	m_WalkingToPos = somePoints[index];
+	navigation_agent_2d.target_position = somePoints[index];
 	
-	
-func _physics_process(delta: float) -> void:
-	move_and_slide()
 
 func SetState(newState: State) -> void:
 	m_State = newState;
@@ -132,6 +146,7 @@ func OnEnteredWalking() -> void:
 	m_GoingToA = !m_GoingToA
 	var points := POINTS_A if m_GoingToA else POINTS_B;
 	SetWalkingPosition(points);
+	
 
 func OnEnteredTalking() -> void:
 	m_Timer = rng.randf_range(MIN_TALK_TIME, MAX_TALK_TIME)
@@ -139,6 +154,9 @@ func OnEnteredTalking() -> void:
 func OnEnteredScared() -> void:
 	pass;
 
+func OnTargetReached() -> void:
+	SetState(State.Walking);
+	
 ## PROCESS STATES ##
 func ProcessIdle(delta: float) -> void:
 	m_Timer -= delta;
@@ -148,12 +166,41 @@ func ProcessIdle(delta: float) -> void:
 	SetState(State.Walking);
 
 func ProcessWalking(delta: float) -> void:
-	var toPos := m_WalkingToPos - global_position;
-	if (toPos.length() <= 20.0):
-		SetState(State.Idle);
-		return;
+	var navWorks: bool = navigation_agent_2d.get_current_navigation_path().size() > 0
+	var target: Vector2 = navigation_agent_2d.get_next_path_position() if navWorks else navigation_agent_2d.target_position
+	var desiredDirection := global_position.direction_to(target)
+	
+	m_Direction = m_Direction.slerp(desiredDirection, ROTATION_SPEED * delta).normalized()
+	
+	var newVelocity := m_Direction * MAX_SPEED
+	if navWorks && navigation_agent_2d.avoidance_enabled:
+		navigation_agent_2d.set_velocity_forced(newVelocity)
+	else:
+		velocity = newVelocity
 		
-	velocity = (m_WalkingToPos - global_position).normalized() * MAX_SPEED
+	if !navWorks:
+		if (global_position.distance_squared_to(target) <= 25.0):
+			OnTargetReached();
 
 func _on_idle_timer_end() -> void:
 	m_State = State.Walking;
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	velocity = safe_velocity
+
+
+func _on_vision_cone_area_body_entered(body: Node2D) -> void:
+	vision_renderer.color = alert_color
+
+
+func _on_vision_cone_area_body_exited(body: Node2D) -> void:
+	vision_renderer.color = vision_color
+
+
+func _on_vision_cone_area_mouse_entered() -> void:
+	vision_renderer.color = alert_color
+
+
+func _on_vision_cone_area_mouse_exited() -> void:
+	vision_renderer.color = vision_color
